c8810de0984b05c223179b368e4a7b216c7a697c
congsun <william9135@gmail.com>
2015-12-02 18:35:38 -0500

    Initial Commit

---
+++ MethodSwizzling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MethodSwizzling
+//
+//  Created by Cong Sun on 12/2/15.
+//  Copyright © 2015 Cong Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MethodSwizzling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MethodSwizzling
+//
+//  Created by Cong Sun on 12/2/15.
+//  Copyright © 2015 Cong Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
